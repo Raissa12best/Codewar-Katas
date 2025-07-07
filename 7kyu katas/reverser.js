4b38d306639648c3adc26e62cd5bb6eb1cff7628
Raissa12best <bestraissa20@gmail.com>
2025-07-07 15:28:31 +0200

    codewar katas

---
+++ 7kyu katas/reverser.js
@@ -0,0 +1,15 @@
+function reverse(n){
+ 
+  let reversed = 0;
+
+  while (n > 0) {
+    let digit = n % 10
+    reversed = reversed * 10 + digit; 
+    n = Math.floor(n / 10);                             
+  }
+
+  return reversed;
+
+
+  
+};
